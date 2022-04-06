7b7964dad1eb6cd0d4dc5f665282d8504cd3ba5f
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-04-05 19:49:52 -0500

    Add files via upload

--- w3school/c_httpModule/demo_querystring.js
+++ w3school/c_httpModule/demo_querystring.js
@@ -2,9 +2,9 @@ const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {'Content-Type' : 'text/html'});
   let q = url.parse(req.url, true).query;
-  let txt = `${q.year} ${q.month}`;
+  let txt = `${q.ano} / ${q.mes}`;
   res.end(txt);
 }).listen(8080);
 
