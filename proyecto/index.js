cc7d820ff383d2027103bbb9ad585dc7efcd73a1
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-04-18 17:59:17 -0500

    Add files via upload

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,1 +1,11 @@
-console.log("hola mundo");
+const app = require("./src/app");
+const mongoose = require("mongoose");
+
+mongoose.promise = global.promise;
+mongoose.connect("mongodb://localhost:27017/facturas", {}).then(()=>{
+    app.listen(3800, ()=>{
+        console.log("servidor iniciado.");
+    });
+}).catch(err => {
+    console.log(err.message);
+});
